b18a919454959ff7b27971fea1f7588ba95372c2
Aadarsh tiwari <aadarshtiwari112@gmail.com>
2025-09-07 17:46:40 +0530

    Create first_lecture.c

---
+++ first_lecture.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    //this id my comment@:)
+    /* this is multi line comment
+    you can write here anything
+    with me
+    */
+    printf("i am a god!\n"); //with new line you can use \n
+    printf("i like my self\n");
+    return 0 ;
+}
