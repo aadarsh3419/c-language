1bdc50b404be1d13b787c756e108e157f9e10645
Aadarsh tiwari <aadarshtiwari112@gmail.com>
2025-09-20 22:20:53 +0530

    this is the pattern of swastic
    this is the pattern of swastic
    user can give number and make the swastic sign

---
+++ swaysthik.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main(){
+    int n,i,j;
+    printf("enter a number greater then 4: ");
+    scanf("%d",&n);
+    for (i=0;i<=n;i++){
+        for (j=0;j<=n;j++){
+            if ( i==n/2 || j==n/2 ){
+                printf("*");
+            }
+            else if (i==0 && j<=n/2){
+                printf("*");
+            }
+            else if ( i == n && j>=n/2){
+                printf("*");
+            }
+            else if ( i<=n/2 && j == n){
+                printf("*");
+
+            }
+            else if ( i >=n/2 && j == 0){
+                printf("*");
+            }
+            else  (printf(" "));
+            
+        }
+    printf("\n");
+    }
+    return 0;
+}
