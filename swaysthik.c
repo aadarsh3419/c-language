8529d9ba55ac6d156957984d256d81b61f62a772
Aadarsh tiwari <aadarshtiwari112@gmail.com>
2025-09-21 20:57:52 +0530

    this one is dimond pattern

--- swaysthik.c
+++ swaysthik.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main(){
     int n,i,j;
-    printf("enter a number greater then 4: ");
+    printf("enter a number: ");
     scanf("%d",&n);
     for (i=0;i<=n;i++){
         for (j=0;j<=n;j++){
