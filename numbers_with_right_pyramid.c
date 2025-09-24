d95c90d3bdaebbc8277d952ef916930c55d6f4a6
Aadarsh tiwari <aadarshtiwari112@gmail.com>
2025-09-24 17:20:36 +0530

    numbers pyramid right

---
+++ numbers_with_right_pyramid.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int n,i,j;
+    printf("enter a number: ");
+    scanf("%d",&n);
+    for (i=0;i<=n;i++){
+        for(j=n;j>=i;j--){
+            printf(" ");}
+        for(j=0;j<=i;j++){
+            printf("%d",j);
+        }
+        
+    printf("\n");
+    }
+}
