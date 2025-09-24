5c9e8c4f4a9f87b39f2c3c35a3e7feaa24f8f656
Aadarsh tiwari <aadarshtiwari112@gmail.com>
2025-09-24 17:54:17 +0530

    alphabat right triangle

---
+++ alphabat_pyramid_right.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int n,i,j;
+    printf("enter a number: ");
+    scanf("%d",&n);
+    for(i=0;i<=n;i++){
+        for(j=0;j<=n-i;j++){
+            printf(" ");
+        }
+        for(j=0;j<=i;j++){
+            printf("%c",65+j);
+        }
+    printf("\n");
+    }
+}
