9946ece988e66bb94bd56239b01bc193db2a3dc6
Aadarsh tiwari <aadarshtiwari112@gmail.com>
2025-10-14 22:00:06 +0530

    k

---
+++ alphabat_hollow_diamound.c
@@ -0,0 +1,18 @@
+    #include<stdio.h>
+    int main(){
+        int n,i,j;
+        printf("enter a number: ");
+        scanf("%d",&n);
+        for (i=0;i<n;i++){
+            for (j=0;j<n;j++){
+                if((i<=n/2 && (j==n/2-i || j==n/2+i))||
+                (i>n/2 && (j==i-n/2 || j==n-1-(i-n/2)))
+                )
+                printf("%c",65+j);
+                else printf(" ");
+            }
+        printf("\n");
+        }
+        
+        return 0;
+    }
---
+++ spiral.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int n,i,j;
+    printf("enter a number: ");
+    scanf("%d",&n);
+    while == n*n{
+        for(i=0;i<=n;i++){
+            for(j=0;j<=n;j++){
+                if(j==n&&i==n||j==0);
+                printf("%d",j);
+            
+        }
+    printf("\n");
+    }}
+}
